1dec12a579d16c2e06548d97d11b4f39355bfe7d
Eric Davis <edavis@eresources.com>
2017-05-28 21:32:29 -0700

    Flesh out the package
    - Add Decimal and Fractional odds formats
    - Add Probability top-level func and Probability methods on all types
    - Add tests
    - Use big.Float to avoid issues with float64s

--- odds.go
+++ odds.go
@@ -1,23 +1,57 @@
+// Package odds provides functionality for working with betting odds.
 package odds
 
-type American int
+import (
+	"math/big"
+)
+
+// Odds is an interface that each odds format implements. It exists so
+// functions can take any of the available odds formats.
+type Odds interface {
+	Probability() *big.Float
+}
+
+// American odds represent either: (1) The stake required to win $100
+// (when negative); or (2) the amount won per $100 staked (when
+// positive).
+type American float64
+
+// Decimal odds represent the amount won (profit + stake) for every $1 staked.
 type Decimal float64
+
+// Fractional odds represent the ratio of profit / stake.
 type Fractional struct {
-	Numerator, Denominator int
-}
-type OddsFormat interface {
-	ToDecimal() float64
+	Profit, Stake float64
 }
 
-func (a American) ToDecimal() float64 {
-	f := float64(a)
-	if f < 0 {
-		return (-f + 100) / -f
+func (a American) Probability() *big.Float {
+	var stake, payout *big.Float
+
+	if i := float64(a); i < 0 {
+		stake = big.NewFloat(float64(-i))
+		payout = big.NewFloat(float64(-i + 100))
 	} else {
-		return (f + 100) / 100
+		stake = big.NewFloat(float64(100))
+		payout = big.NewFloat(float64(i + 100))
 	}
+
+	return stake.Quo(stake, payout)
 }
 
-func (a American) Prob() float64 {
-	return 1 / a.ToDecimal()
+func (d Decimal) Probability() *big.Float {
+	stake := big.NewFloat(1.0)
+	odds := big.NewFloat(float64(d))
+	stake.Quo(stake, odds)
+	return stake
+}
+
+func (f Fractional) Probability() *big.Float {
+	stake := big.NewFloat(float64(f.Stake))
+	payout := big.NewFloat(float64(f.Profit + f.Stake))
+	return stake.Quo(stake, payout)
+}
+
+// Probability returns the implied probability of the given odds.
+func Probability(o Odds) *big.Float {
+	return o.Probability()
 }
--- odds_test.go
+++ odds_test.go
@@ -5,20 +5,50 @@ import (
 	"testing"
 )
 
-func TestAmerican(t *testing.T) {
-	var tests = []struct {
-		original int
-		decimal  float64
-		prob     float64
+const (
+	prec = 4
+)
+
+func TestProbability(t *testing.T) {
+	tests := []struct {
+		odds Odds
+		prob string
 	}{
-		{-139, 1.719, 0.5817},
-		{+126, 2.260, 0.4424},
-		{-170, 1.588, 0.6297},
-		{+160, 2.600, 0.3846},
+		{American(-600), "0.8571"},
+		{American(-300), "0.75"},
+		{American(-250), "0.7143"},
+		{American(-200), "0.6667"},
+		{American(-150), "0.6"},
+		{American(-110), "0.5238"},
+		{American(+100), "0.5"},
+		{American(+150), "0.4"},
+		{American(+200), "0.3333"},
+		{American(+250), "0.2857"},
+		{American(+300), "0.25"},
+		{American(+450), "0.1818"},
+
+		{Decimal(5.0), "0.2"},
+		{Decimal(4.0), "0.25"},
+		{Decimal(3.0), "0.3333"},
+		{Decimal(2.5), "0.4"},
+		{Decimal(2.0), "0.5"},
+		{Decimal(1.9090), "0.5238"},
+		{Decimal(1.5), "0.6667"},
+		{Decimal(1.1), "0.9091"},
+		{Decimal(1.0), "1"},
+
+		{Fractional{10, 1}, "0.09091"},
+		{Fractional{6, 1}, "0.1429"},
+		{Fractional{2, 1}, "0.3333"},
+		{Fractional{10, 11}, "0.5238"},
+		{Fractional{2, 5}, "0.7143"},
+		{Fractional{2.5, 7}, "0.7368"},
+		{Fractional{1, 3}, "0.75"},
+		{Fractional{8, 11}, "0.5789"},
 	}
-	for _, obj := range tests {
-		a := American(obj.original)
-		assert.InEpsilon(t, a.ToDecimal(), obj.decimal, 0.001)
-		assert.InEpsilon(t, a.Prob(), obj.prob, 0.001)
+
+	for _, test := range tests {
+		p := Probability(test.odds)
+		assert.Equal(t, test.prob, p.Text('g', prec))
 	}
 }
